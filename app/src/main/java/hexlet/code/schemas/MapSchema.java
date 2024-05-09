8f5b47a29aee62a044a2e6bea424f8a5c50dc86b
Ярослав Родионов <iaroslav@MacBook-Pro--Iaroslav.local>
2024-05-09 13:10:38 +0300

    update

--- app/src/main/java/hexlet/code/schemas/MapSchema.java
+++ app/src/main/java/hexlet/code/schemas/MapSchema.java
@@ -3,10 +3,9 @@ package hexlet.code.schemas;
 import java.util.Map;
 import java.util.HashMap;
 
-public class MapSchema extends BaseSchema<Map<String, Object>> {
+public class MapSchema extends BaseSchema<Map<String, String>> {
     private Integer requiredSize = null;
-    // Используйте Map<String, ? extends BaseSchema<?>> для более гибкой совместимости
-    private Map<String, ? extends BaseSchema<?>> shapeSchemas = new HashMap<>();
+    private Map<String, ? extends BaseSchema<String>> shapeSchemas = new HashMap<>();
 
     public final MapSchema required() {
         super.required();
@@ -18,23 +17,21 @@ public class MapSchema extends BaseSchema<Map<String, Object>> {
         return this;
     }
 
-    // Принимайте Map<String, ? extends BaseSchema<?>> для большей совместимости с тестами
-    public final MapSchema shape(Map<String, ? extends BaseSchema<?>> schemas) {
+    public final MapSchema shape(Map<String, ? extends BaseSchema<String>> schemas) {
         this.shapeSchemas = schemas;
         return this;
     }
 
     @Override
-    protected final boolean checkAdditionalConditions(Map<String, Object> value) {
+    protected final boolean checkAdditionalConditions(Map<String, String> value) {
         if (requiredSize != null && (value == null || value.size() != requiredSize)) {
             return false;
         }
-        // Проверка на соответствие каждому ключу согласно определённой схеме
         if (!shapeSchemas.isEmpty()) {
-            for (Map.Entry<String, ? extends BaseSchema<?>> entry : shapeSchemas.entrySet()) {
+            for (Map.Entry<String, ? extends BaseSchema<String>> entry : shapeSchemas.entrySet()) {
                 String key = entry.getKey();
-                BaseSchema<?> schema = entry.getValue();
-                Object val = value != null ? value.get(key) : null;
+                BaseSchema<String> schema = entry.getValue();
+                String val = value != null ? value.get(key) : null;
 
                 if (!value.containsKey(key) || !schema.validate(val)) {
                     return false;
