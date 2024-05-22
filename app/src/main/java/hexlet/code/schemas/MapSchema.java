c210673ac0730f8229564115dc8ec4ffb0c0f257
Ярослав Родионов <iaroslav@MacBook-Pro--Iaroslav.local>
2024-05-22 18:37:36 +0300

    update 2.4

--- app/src/main/java/hexlet/code/schemas/MapSchema.java
+++ app/src/main/java/hexlet/code/schemas/MapSchema.java
@@ -10,18 +10,32 @@ public class MapSchema extends BaseSchema<Map<String, Object>> {
     private Integer requiredSize = null;
     private Map<String, BaseSchema<?>> shapeSchemas = new HashMap<>();
 
+    /**
+     * Устанавливает эту схему как обязательную.
+     * @return экземпляр этой схемы
+     */
     public MapSchema required() {
         super.required();
         addCheck(value -> value != null && value instanceof Map);
         return this;
     }
 
+    /**
+     * Устанавливает размер карты для проверки.
+     * @param size требуемый размер карты
+     * @return экземпляр этой схемы
+     */
     public MapSchema sizeof(int size) {
         this.requiredSize = size;
         addCheck(value -> value.size() == size);
         return this;
     }
 
+    /**
+     * Определяет набор схем для проверки значений каждого ключа карты.
+     * @param schemas карта схем для проверки
+     * @return экземпляр этой схемы
+     */
     public MapSchema shape(Map<String, BaseSchema<?>> schemas) {
         this.shapeSchemas = new HashMap<>(schemas);
         addCheck(value -> {
